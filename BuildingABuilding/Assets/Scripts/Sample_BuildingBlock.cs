2c3abe5e080da7ec601eef64e410964d0f540067
WangRimen <34192438+WangRimen@users.noreply.github.com>
2018-09-05 11:00:39 -0400

    Add UI
    Add UI

--- BuildingABuilding/Assets/Scripts/Sample_BuildingBlock.cs
+++ BuildingABuilding/Assets/Scripts/Sample_BuildingBlock.cs
@@ -3,59 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Sample_BuildingBlock : MonoBehaviour {
-    public float horizontal_force = 2.0f;
-    public float extra_gravity = 9.81f;
 
-    public GameObject[] building_list;
-    public Material[] color_list;
-    public float spawn_time =0.8f;
-    public GameObject building_block;
-    public float launchspeed_multiplier=1.5f;
-    public float launchspeed_vertical_multiplier = 0.5f;
+    public bool released = false;
 
-    
-    // Use this for initialization
-    private Rigidbody rb;
-    
-	void Start () {
-        rb = GetComponent<Rigidbody>();
-	}
-
-
-
-    private void FixedUpdate()
+    private void OnCollisionEnter(Collision collision)
     {
-        rb.velocity += Vector3.down * extra_gravity;
-
-        rb.velocity += Vector3.right * horizontal_force * Input.GetAxis("Horizontal");
-
-
-        if (Input.GetKey("space"))
+        if (gameObject.layer != 8 && released )
         {
-            if (building_block != null)
-            {
-
-                building_block.GetComponent<Rigidbody>().isKinematic = false;
-
-                Vector3 launch_speed = rb.velocity * launchspeed_multiplier;
-                launch_speed.y = rb.velocity.y* launchspeed_vertical_multiplier;
-                building_block.GetComponent<Rigidbody>().velocity = launch_speed;
-                building_block.transform.parent = null;
-                building_block.GetComponent<BoxCollider>().enabled = true;
-
-                building_block = null;
-                StartCoroutine("SpawnBuildingBlock");
-
-            }
+            if (collision.gameObject.layer == 8)
+                gameObject.layer = 8; 
         }
-    }
-
-    IEnumerator SpawnBuildingBlock()
-    {
-
-        yield return new WaitForSeconds(spawn_time);
         
-        building_block = Instantiate(building_list[Random.Range(0,building_list.Length)], transform);
-        building_block.GetComponent<MeshRenderer>().material = color_list[Random.Range(0, color_list.Length)];
     }
 }
---
+++ BuildingABuilding/Assets/Scripts/Sample_HeightDetection.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sample_HeightDetection : MonoBehaviour {
+
+    public Text height_display;
+    public LayerMask targetLayer;
+
+    public LineRenderer vertical_line;
+    public LineRenderer horizontal_line;
+    private void Update()
+    {
+
+        Vector3 origin = transform.position + Vector3.up*5000;
+        Vector3 boxcast_size = transform.localScale/2;
+
+        RaycastHit hit;
+
+        if (Physics.BoxCast(origin, boxcast_size, Vector3.down, out hit, new Quaternion(), 6000, targetLayer))
+        {
+
+            vertical_line.enabled = true;
+            horizontal_line.enabled = true;
+
+            height_display.text = "Height: " + hit.point.y.ToString();
+
+
+            horizontal_line.SetPosition(0, hit.point);
+            Vector3 line_end = hit.point;
+            line_end.x = -transform.localScale.x / 2;
+            horizontal_line.SetPosition(1, line_end);
+
+            vertical_line.SetPosition(0, line_end);
+            line_end.y = 0;
+            vertical_line.SetPosition(1, line_end);
+
+
+
+        }
+        else
+        {
+            vertical_line.enabled = false;
+            horizontal_line.enabled = false;
+        }
+    }
+}
